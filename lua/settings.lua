097f90a1564d5ff1511b56edf7d1f696946cd131
huik <kevin.hui@dowjones.com>
2023-10-14 13:48:17 +0100

    feat: Use expandtab

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,6 +6,7 @@ set number hidden ignorecase incsearch smartcase showmatch autoindent ruler vb n
 set mouse=a
 set history=1000
 set undolevels=1000
-set tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 
+" set tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 
+set expandtab shiftwidth=2
 set clipboard=unnamed
 ]])
